e718380043fb2d26e61f3945c3797d67460fd3e0
Matthieu Dolez <matthieu.dolez@web-atrio.com>
2021-11-19 16:54:20 +0100

    Added spring initializer project

---
+++ src/main/java/fr/glowingenigma/app/GlowingEnigmaApplication.java
@@ -0,0 +1,13 @@
+package fr.glowingenigma.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowingEnigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowingEnigmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/glowingenigma/app/GlowingEnigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.glowingenigma.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowingEnigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
