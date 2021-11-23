15f347243fcf6e3bbc923a1639d049f3526af113
Matthieu Dolez <matthieu.dolez@web-atrio.com>
2021-11-23 10:52:22 +0100

    added request mapping to back-end

--- src/main/java/fr/glowingenigma/GlowingEnigmaApplication.java
+++ src/main/java/fr/glowingenigma/GlowingEnigmaApplication.java
@@ -1,4 +1,4 @@
-package fr.glowingenigma.app;
+package fr.glowingenigma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/fr/glowingenigma/web/pages/MainController.java
@@ -0,0 +1,12 @@
+package fr.glowingenigma.web.pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(value = {"/", "/login"})
+    public String entry(){
+        return "index";
+    }
+}
